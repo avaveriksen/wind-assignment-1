7f9eb28182b811fa7a6fe3241614bc9679955861
Anders Eriksen <averiksen@proton.me>
2025-09-18 17:10:05 +0200

    Interpolation across tc in toolbox
    Got interpolation across tables working, tables are stored in interpolated-tables

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from windtools import tools # Our toolbox of helper functions
+from windtools import Tools, Interpolations # Our toolbox of helper functions
 
 if __name__ == '__main__':
     '''main loop'''
+
+    # Don't run this always, comment out when we have out tables
+    Interpolations.interp_tc() # Interpolate tables across tc
+
     #pn, pt, a, aprime, F = tools.bem_single_element(PROVIDE PARAMETERS)
+
--- windtools.py
+++ windtools.py
@@ -1,6 +1,8 @@
 import numpy as np
+import os
+import pandas as pd
 
-class tools:
+class Tools:
 
     def bem_single_element(r, c, beta, V0, omega, theta_p, Cl, Cd, R=89.17, B=3):
         '''Compute the BEM algorithm for a single element'''
@@ -50,3 +52,61 @@ class tools:
         pn = 0.5 * rho * (Vrel ** 2) * c * Cn
         pt = 0.5 * rho * (Vrel ** 2) * c * Ct
         return pn, pt, a, aprime, F
+
+class Interpolations:
+    def interp_tc(self):
+        # data provided
+        files = ["FFA-W3-241.txt",
+                 "FFA-W3-301.txt",
+                 "FFA-W3-360.txt",
+                 "FFA-W3-480.txt",
+                 "FFA-W3-600.txt",
+                 "cylinder.txt"]
+        # accompanying tc
+        tc_list = [0.241,
+                   0.301,
+                   0.360,
+                   0.480,
+                   0.600,
+                   1]
+        # including tc's in 10MW blade
+        tc_extended = [0.241,
+                       0.2426,
+                       0.2532,
+                       0.2781,
+                       0.3242,
+                       0.4304,
+                       0.611,
+                       0.8605,
+                       1]
+        # data in one list [(tc, dataframe), (tc, dataframe)...]
+        airfoil_data = []
+
+        for i, tc in enumerate(tc_list):
+            # fill airfoil_data with (tc, dataframe) tuples
+            df = pd.read_csv(files[i], delimiter='\t', header=None, names=['alpha', 'cl', 'cd', 'cm'])
+            airfoil_data.append((tc, df))
+
+        for i, tc in enumerate(tc_extended):
+            # The tc's for which were not provided tabels are linearly interpolated
+            if not (tc in tc_list):
+                # variables for linear slope calculation
+                tc1 = airfoil_data[i - 1][0]
+                df1 = airfoil_data[i - 1][1]
+                tc2 = airfoil_data[i][0]
+                df2 = airfoil_data[i][1]
+
+                # linear interpolation
+                df_interp = df1 + (tc - tc1) * ((df2 - df1) / (tc2 - tc1))
+
+                # round values in dataframe
+                df_interp = df_interp.__round__(4)
+
+                # chop up, insert and concatenate airfoil_data with interp
+                arr = airfoil_data[:i] + [(tc, df_interp)] + airfoil_data[i:]
+                airfoil_data = arr
+
+        for tup in airfoil_data:
+            # save to csv files
+            file_name = os.getcwd() + "\\interpolated-tables\\FFA_W3-" + str(round(tup[0], 4)) + ".csv"
+            tup[1].to_csv(file_name, index=False)
