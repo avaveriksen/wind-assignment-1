9a176be4578faef3a7482836f5404a6bc680eaa1
Anders Eriksen <averiksen@proton.me>
2025-09-22 13:46:19 +0200

    updated bladedata

--- main.py
+++ main.py
@@ -5,15 +5,6 @@ if __name__ == '__main__':
 
     # Don't run this always, comment out when we have out tables
     #Interpolations.interp_tc() # Interpolate tables across tc
+    #Tools.extend_bladedata()
 
-    #pn, pt, a, aprime, F = tools.bem_single_element(PROVIDE PARAMETERS)
 
-    r=88.45
-    c=1.18
-    beta=-3.36
-    V0= 4.0 # m/s
-    omega=0.226116 # rad/s
-    theta_p=-4.0 # degrees
-    file = 'interpolated-tables\FFA_W3-0.241.csv'
-
-    pn, pt, a, aprime, F = Tools.bem_single_element(r,c,beta,V0,omega,theta_p,file) # Run BEM on a single element, provide parameters within the function
